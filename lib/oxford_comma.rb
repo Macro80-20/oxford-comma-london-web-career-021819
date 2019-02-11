0f3648cc6032f2e8bcc3dc5f1ffcf46652062a06
fernando <fernando_lopezc@live.com>
2019-02-11 20:13:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ elsif array.length == 3
   string = array.join(", ")
   string.insert(14,"and ")
 elsif array.length > 3
-  array[-1].prepend " and"
+  array[-1].prepend " and "
   array.join(", ")
   else
   string = array.join
