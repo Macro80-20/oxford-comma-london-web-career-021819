923a027b30d93c94f05506882625c903375115c4
fernando <fernando_lopezc@live.com>
2019-02-11 20:04:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ elsif array.length == 3
   string.insert(14,"and ")
 elsif array.length > 3
   string = array.join(", ")
-  string.insert(35,"and ")
+  string.insert(31,"and ")
   else
   string = array.join
 end
