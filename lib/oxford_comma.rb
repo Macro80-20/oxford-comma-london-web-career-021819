da1777ea196ae2d4ff90e83815d81cf0af5a3713
fernando <fernando_lopezc@live.com>
2019-02-11 20:00:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
 if array.length == 2
   array.join(" and ")
-else
+elsif array.length == 3
+  string = arr.join(" ,")
+  string.insert(10,"and")
+  else
   string = array.join
 end
 end
